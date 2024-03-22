d9111d84ee1c86610083a92a26b9ffed48635a5c
plusSign <plus@vip.qq.com>
2024-03-22 17:26:31 +0800

    a minimum demo to reproduct signer.sendTransaction() exception issues

--- src/App.js
+++ src/App.js
@@ -1,9 +1,91 @@
 import "./App.css";
-import { usePrivy } from "@privy-io/react-auth";
+import { usePrivy, useWallets } from "@privy-io/react-auth";
+import { ethers } from "ethers";
+
+const errorTest = (wallets)=>{
+  return new Promise(async (resolve, reject) => {
+    try {
+      const embeddedWallet = wallets.find((wallet) => (wallet.walletClientType === 'privy'));
+      await embeddedWallet.switchChain(137);
+      
+      const provider = await embeddedWallet.getEthersProvider();
+      const signer = provider.getSigner(); // ethers signer object
+
+      const gasPrice = await signer.getGasPrice();
+      console.log('getGasPrice ' + gasPrice);
+
+      const _maxFeePerGas = gasPrice.mul(2); 
+      const _maxPriorityFeePerGas = _maxFeePerGas; 
+
+      signer.sendTransaction({
+        maxFeePerGas: _maxFeePerGas,
+        maxPriorityFeePerGas: _maxPriorityFeePerGas,
+        to: embeddedWallet.address,
+        value: ethers.utils.parseUnits("1", "gwei")
+      }).then(res => {
+        console.log(res);
+        resolve(res);
+      }).catch(e => {
+          console.log("[exception]" + e);
+          reject(e);
+      });
+    } 
+    catch(e){
+      console.log("[exception]" + e);
+      if (e.message.indexOf('(reading \'switchChain\')') > 0) {
+        console.log("privy need login")
+        reject('need login');
+      } else {
+        reject(e);
+      }
+    }
+  })
+}
+
+const worksTest = (wallets) => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      const embeddedWallet = wallets.find((wallet) => (wallet.walletClientType === 'privy'));
+      await embeddedWallet.switchChain(137);
+      
+      const provider = await embeddedWallet.getEthersProvider();
+      const signer = provider.getSigner(); // ethers signer object
+
+      const gasPrice = await signer.getGasPrice();
+      console.log('getGasPrice ' + gasPrice);
+
+      //const _maxFeePerGas = gasPrice.mul(2); 
+      //const _maxPriorityFeePerGas = _maxFeePerGas; 
+
+      signer.sendTransaction({
+        //maxFeePerGas: _maxFeePerGas,
+        //maxPriorityFeePerGas: _maxPriorityFeePerGas,
+        to: embeddedWallet.address,
+        value: ethers.utils.parseUnits("1", "gwei")
+      }).then(res => {
+        console.log(res);
+        resolve(res);
+      }).catch(e => {
+          console.log("[exception]" + e);
+          reject(e);
+      });
+    } 
+    catch(e){
+      console.log("[exception]" + e);
+      if (e.message.indexOf('(reading \'switchChain\')') > 0) {
+        console.log("privy need login")
+        reject('need login');
+      } else {
+        reject(e);
+      }
+    }
+  })
+}
 
 function App() {
   const { ready, authenticated, user, login, logout } = usePrivy();
 
+  const {wallets} = useWallets();
   // Wait until the Privy client is ready before taking any actions
   if (!ready) {
     return null;
@@ -25,6 +107,24 @@ function App() {
             <button onClick={logout} style={{ marginTop: "20px", padding: "12px", backgroundColor: "#069478", color: "#FFF", border: "none", borderRadius: "6px" }}>
               Log Out
             </button>
+            <br/>
+
+
+            <button onClick={async()=>{
+              await errorTest(wallets);
+            }} style={{ marginTop: "20px", padding: "12px", backgroundColor: "#069478", color: "#FFF", border: "none", borderRadius: "6px" }}>
+              error Test
+            </button>
+            <br/>
+
+
+            <button onClick={async()=>{
+              await worksTest(wallets);
+            }} style={{ marginTop: "20px", padding: "12px", backgroundColor: "#069478", color: "#FFF", border: "none", borderRadius: "6px" }}>
+              works Test
+            </button>
+            <br/>
+
           </div>
         ) : (
           <button onClick={login} style={{padding: "12px", backgroundColor: "#069478", color: "#FFF", border: "none", borderRadius: "6px" }}>Log In</button>
--- src/index.js
+++ src/index.js
@@ -4,12 +4,38 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { PrivyProvider } from '@privy-io/react-auth';
+import { addRpcUrlOverrideToChain } from '@privy-io/react-auth';
+
+import { polygon } from 'viem/chains';
+
+const PolygonRPC = 'https://polygon-mainnet.g.alchemy.com/v2/X_2ZBjbSXqwuFzBj7fVWmLhaBnGA0Hff';
+const polygonChain = addRpcUrlOverrideToChain(polygon, PolygonRPC);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <PrivyProvider
       appId={process.env.REACT_APP_PRIVY_APP_ID}
+      config={
+          {
+              appearance:
+                  {
+                      "accentColor": "#6A6FF5",
+                      "theme": "dark",
+                      "showWalletLoginFirst": true,
+                      "logo": 'https://www.turnup.so/logo512.png'
+                  },
+              //loginMethods: ["google", "apple", "email"],
+              captchaEnabled : false,
+              defaultChain: polygonChain,
+              supportedChains:[polygonChain],
+              embeddedWallets: {
+                  createOnLogin: "all-users",
+                  noPromptOnSignature: true, // defaults to false
+                  requireUserPasswordOnCreate: false
+              }
+          }
+      }
       onSuccess={(user) => console.log(`User ${user.id} logged in!`)}
     >
       <App />
